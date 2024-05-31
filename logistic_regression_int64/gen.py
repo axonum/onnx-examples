e9c0b968217b9de09e1c9e5aeede3965cb32f044
axonon <anon@axonum.io>
2024-05-31 14:39:07 +0200

    feat: Add Logistic Regression contract for performing logistic regression inference
    This commit adds a new contract called LRContract, which is responsible for performing logistic regression inference. It includes a `logisticRegression` function that takes an array of int64 inputs and returns a single int64 output after performing the inference using a pre-defined ONNX model. The contract also includes a helper function `encodeArray` to convert the input array into bytes format. Additionally, an event `LogisticRegression` is emitted with the input bytes and output value. This contract is useful for performing logistic regression predictions in a decentralized manner.

--- logistic_regression_int64/gen.py
+++ logistic_regression_int64/gen.py
@@ -12,7 +12,10 @@ class LogisticRegression(torch.nn.Module):
         self.linear = torch.nn.Linear(10, 1)
 
     def forward(self, x):
-        x =  torch.sigmoid(self.linear(x))
+        # ! make sure the input can fit into float32
+        x = x.type(torch.float32)
+        x = self.linear(x)
+        x = torch.sigmoid(x)
         # Round the output to 0 or 1
         return torch.add(torch.mul(x >= 0.5, 1), torch.mul(x < 0.5, 0)).type(torch.int64)
     
@@ -21,7 +24,7 @@ model = LogisticRegression()
 model.eval()
 
 # Export the model
-input = torch.randn(1, 10)
+input = torch.tensor([[-5,-4,-3,-2,-1,1,2,3,4,5]], dtype=torch.int64)
 torch.onnx.export(model, input, "model.onnx", export_params=True, opset_version=7, do_constant_folding=True, input_names = ['input'], output_names = ['output'], verbose=False)
 
 # Export sample input and output
